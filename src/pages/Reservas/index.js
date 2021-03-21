acffb21cfe45e5948d43816e0934712327ca56e5
Lizandro Kenedy <lizandro.santos@grupolpj.com>
2021-03-21 13:49:06 -0400

    Estilizando componenete reservas

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { MdDelete } from 'react-icons/md'
+import './style.css'
 
 export default function Reservas() {
   return (
     <div>
-      <h1>Reservas</h1>
+      <h1>Você solicitou 1 reserva</h1>
+
+      <div className="reservas">
+        <img src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg" alt="Maceio" />
+        <strong>Viagem Maceio 7 Dias</strong>
+        <span>Quantidade: 2</span>
+        <button type="button" onClick={() => { }}>
+          <MdDelete size={20} color="#191919" />
+        </button>
+      </div>
+
+      <footer>
+        <button type="button">Solicitar Reservas</button>
+      </footer>
     </div>
   )
 }
